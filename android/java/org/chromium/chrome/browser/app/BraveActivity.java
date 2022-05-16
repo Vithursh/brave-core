d28160ea7d11bcf24e831d977926f686a49972a3
samartnik <artem@brave.com>
2022-05-16 08:32:59 -0400

    [Android] Clean up toolbar hairline code
    Chromium change:
    https://github.com/chromium/chromium/commit/d7d355972b5c6ac82234550a2859cdc6ec4f894c
    
    [GMNext] Clean up toolbar hairline code
    Bug: 1227450

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -1404,7 +1404,7 @@ public abstract class BraveActivity<C extends ChromeActivityComponent> extends C
     }
 
     public int getToolbarShadowHeight() {
-        View toolbarShadow = findViewById(R.id.toolbar_shadow);
+        View toolbarShadow = findViewById(R.id.toolbar_hairline);
         assert toolbarShadow != null;
         if (toolbarShadow != null) {
             return toolbarShadow.getHeight();
@@ -1413,7 +1413,7 @@ public abstract class BraveActivity<C extends ChromeActivityComponent> extends C
     }
 
     public int getToolbarBottom() {
-        View toolbarShadow = findViewById(R.id.toolbar_shadow);
+        View toolbarShadow = findViewById(R.id.toolbar_hairline);
         assert toolbarShadow != null;
         if (toolbarShadow != null) {
             return toolbarShadow.getBottom();
